3deccb18c42d95fdf4aad62283cdea4150d8a1b3
Chiqi84 <131625283+Chiqi84@users.noreply.github.com>
2023-04-27 01:48:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -88,11 +88,11 @@ app.get('/gpt/:text', async (req, res) => {
         messages.push({role: "assistant", content: agent_response})
 
         //Check for Twitch max. chat message length limit and slice if needed
-        if(agent_response.length > 399){
-          console.log("Agent answer exceeds twitch chat limit. Slicing to first 399 characters.")
-          agent_response = agent_response.substring(0, 399)
-          console.log ("Sliced agent answer: " + agent_response)
-        }
+        //if(agent_response.length > 399){
+        //  console.log("Agent answer exceeds twitch chat limit. Slicing to first 399 characters.")
+        //  agent_response = agent_response.substring(0, 399)
+        //  console.log ("Sliced agent answer: " + agent_response)
+        //}
 
         res.send(agent_response)
       } else {
